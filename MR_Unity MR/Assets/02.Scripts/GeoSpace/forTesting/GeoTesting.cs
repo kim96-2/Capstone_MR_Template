c397784a60aa5c64f288eec1e93f2d90b093d091
Lee <totolikid@naver.com>
2024-04-10 23:53:30 +0900

    Unit/Meter 확인용 테스트 스크립트 + 씬 추가

---
+++ MR_Unity MR/Assets/02.Scripts/GeoSpace/forTesting/GeoTesting.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class GeoTesting : MonoBehaviour
+{
+    [SerializeField] InputActionReference triggerInputReference;
+
+    [SerializeField] GameObject leftController;
+    [SerializeField] GameObject rightController;
+
+    [SerializeField] TMP_Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(triggerInputReference.action.ReadValue<float>() > 0)
+        {
+            float dis = (leftController.transform.position - rightController.transform.position).magnitude;
+            text.text = "Distance : " + dis.ToString("F2");
+        }
+    }
+}
