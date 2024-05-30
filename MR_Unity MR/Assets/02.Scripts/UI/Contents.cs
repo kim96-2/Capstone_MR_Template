a4732e0b8fd85bbf29977eebc4e7ddd409d4acb0
ab3631 <43373886+ab3631@users.noreply.github.com>
2024-05-30 14:55:36 +0900

    5/30
    dd

--- MR_Unity MR/Assets/02.Scripts/UI/Contents.cs
+++ MR_Unity MR/Assets/02.Scripts/UI/Contents.cs
@@ -10,10 +10,11 @@ public class Contents : MonoBehaviour
 {
 
     [SerializeField] private Place info;
-    [SerializeField] private TextMeshProUGUI nameText;
+    [SerializeField] private TextMeshProUGUI place_nameText;
+    [SerializeField] private TextMeshProUGUI category_nameText;
     [SerializeField] private TextMeshProUGUI phoneText;
-    [SerializeField] private TextMeshProUGUI addressText;
-    [SerializeField] private TextMeshProUGUI road_addressText;
+    [SerializeField] private TextMeshProUGUI address_nameText;
+    [SerializeField] private TextMeshProUGUI road_address_nameText;
 
     void Start()
     {
@@ -30,10 +31,11 @@ public class Contents : MonoBehaviour
 
     public void changeContents(Place place)
     {
-        info.place_name = nameText.text = place.place_name;
+        info.place_name = place_nameText.text = place.place_name;
+        info.category_name = category_nameText.text = place.category_name;
         info.phone = phoneText.text = place.phone;
-        info.address_name = addressText.text = place.address_name;
-        info.road_address_name = road_addressText.text = place.road_address_name;
+        info.address_name = address_nameText.text = place.address_name;
+        info.road_address_name = road_address_nameText.text = place.road_address_name;
     }
 
     public void clickThis() { InformationUI.Instance.MoreInfo(info); }
