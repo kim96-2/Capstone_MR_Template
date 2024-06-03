76a0457dd1dd9737d8990097b9bf83427f2a8756
jaehyun9912 <limit8802@gmail.com>
2024-06-03 21:07:38 +0900

    위치서비스 클래스 구현

--- MR_Unity MR/Assets/02.Scripts/GPS.cs
+++ MR_Unity MR/Assets/02.Scripts/GPS.cs
@@ -3,17 +3,40 @@ using UnityEngine;
 
 public class GPS : MonoBehaviour
 {
-    private void Start()
+    public bool locationEnabled { get; private set; } = false;
+    public float latitude { get; private set; }
+    public float longitude { get; private set; }
+    public float altitude { get; private set; }
+    public float accuracy { get; private set; }
+
+    void Start()
     {
-        // 위치 서비스 활성화 여부 확인
+        // 현재 디바이스 상 위치 서비스 활성화 여부 확인
         if (!Input.location.isEnabledByUser)
         {
-            Debug.LogError("위치 서비스가 활성화되어 있지 않습니다.");
+            Debug.LogError("Location services are not enabled on the device.");
             return;
         }
-        
-        Input.location.Start();
-        
-        
+
+        // Request location updates
+        Input.location.Start(5f, 10f);
+    }
+
+    void Update()
+    {
+        // Check if we have a new location update
+        if (Input.location.status == LocationServiceStatus.Running)
+        {
+            latitude = Input.location.lastData.latitude;
+            longitude = Input.location.lastData.longitude;
+            altitude = Input.location.lastData.altitude;
+            accuracy = Input.location.lastData.horizontalAccuracy;
+        }
+        // 위치 서비스 다시 시도
+        else if (Input.location.isEnabledByUser)
+        {
+            // Request location updates
+            Input.location.Start(5f, 10f);
+        }
     }
 }
